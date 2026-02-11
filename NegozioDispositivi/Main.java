53a2b3bd864b8af93b851b8ad802da8e0209ad73
Nemesis315 <cmatteo.conte@gmail.com>
2026-02-11 09:30:00 +0000

    update

--- NegozioDispositivi/Main.java
+++ NegozioDispositivi/Main.java
@@ -1,17 +1,21 @@
 package NegozioDispositivi;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main 
+{
+    public static void main(String[] args) 
+    {
         Dispositivo[] dispositivi = new Dispositivo[2];
         dispositivi[0] = new Smartphone("Samsung", 2);
         dispositivi[1] = new Tablet("Apple", true);
 
-        for (Dispositivo d : dispositivi) {
+        for (Dispositivo d : dispositivi) 
+        {
             d.mostraInfo();
         }
 
         // Dimostrazione dell'overloading su Smartphone
-        if (dispositivi[0] instanceof Smartphone) {
+        if (dispositivi[0] instanceof Smartphone) 
+        {
             Smartphone s = (Smartphone) dispositivi[0];
             s.chiama();
             s.chiama("Luca");
